4445c582f451ca7b17886545840a6bc5b113614f
abhi8960git <109978720+abhi8960git@users.noreply.github.com>
2024-10-05 21:39:44 +0530

    Create FlashLoan.sol

---
+++ FlashLoan.sol
@@ -0,0 +1,83 @@
+
+File explorer
+
+Remix
+The Native IDE for Web3 Development.
+WebsiteRemix Desktop
+Explore. Prototype. Create.
+Recent Workspaces
+Semaphore - 1
+default_workspace
+Files
+Featured
+
+    WATCH TO LEARN
+
+    Video Tips from the Remix Team
+    Remix has a growing library of videos containing lots of tips for using the tool. Check them out and subscribe to get our latest uploads.
+    Watch
+    BETA TESTING
+
+    Our community supports us.
+    Help us beta test releases now and get a handle on new features!
+    Sign up
+    v0.54.0 RELEASE HIGHLIGHTS
+        Login to GitHub from 'File Explorer'
+        Added Cookbook workspace templates
+        Added 'sendRawTransaction' API to remix-simulator
+    Read More
+
+LearnEth: Tutorials inside Remix
+Check out tutorials on Remix, Solidity, and other Web3 projects. Great for all skill levels.
+Start Learning
+WATCH TO LEARN
+
+Video Tips from the Remix Team
+Remix has a growing library of videos containing lots of tips for using the tool. Check them out and subscribe to get our latest uploads.
+Watch
+BETA TESTING
+
+Our community supports us.
+Help us beta test releases now and get a handle on new features!
+Sign up
+v0.54.0 RELEASE HIGHLIGHTS
+
+    Login to GitHub from 'File Explorer'
+    Added Cookbook workspace templates
+    Added 'sendRawTransaction' API to remix-simulator
+
+Read More
+
+    LearnEth: Tutorials inside Remix
+    Check out tutorials on Remix, Solidity, and other Web3 projects. Great for all skill levels.
+    Start Learning
+
+Featured Plugins
+
+0
+Listen on all transactions
+ Welcome to Remix 0.54.1 
+
+Your files are stored in indexedDB, 8.82 MB / 10 GB used
+
+You can use this terminal to: 
+
+    Check transactions details and start debugging.
+    Execute JavaScript scripts:
+     - Input a script directly in the command line interface 
+     - Select a Javascript file in the file explorer and then run `remix.execute()` or `remix.exeCurrent()`  in the command line interface 
+     - Right click on a JavaScript file in the file explorer and then click `Run` 
+
+The following libraries are accessible:
+
+    web3.js
+    ethers.js 
+    sol-gpt <your Solidity question here>  
+
+Type the library name to see available commands.
+>
+Initialize as git repo
+Did you know?
+You can use the help of AI for Solidity error, click on 'Ask RemixAI'.
+RemixAI Copilot (enabled)
+Scam Alert
